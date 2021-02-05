e70f90b8041bea98602811def5656eba02d40cb5
RommanSh <rshatskih@gmail.com>
2021-02-05 11:04:23 +0300

    Обновлен script.js
    добавлен код для адаптации main-slider и slider под другие разрешения

--- src/js/script.js
+++ src/js/script.js
@@ -6,18 +6,63 @@ $(document).ready(function(){
         dots: true,
         slidesToShow: 3,
         slidesToScroll: 1,
-        speed: 1000,
+        speed: 900,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/prev-main.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/next-main.svg"></button>'
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/next-main.svg"></button>',
+        
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    speed: 600,
+                    arrows: false
+                }
+
+            },
+            {
+                breakpoint: 577,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false
+                }
+
+            }
+        ]
     });
 
     /* Slider result.html, blocker.html, hologram.html, security.html, battary.html, clean.html, login.html, smart.html */
 
     $('.slider__inner').slick({
+        dots: false,
         slidesToShow: 5,
         slidesToScroll: 1,
         speed: 1000,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/prev.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/next.svg"></button>'
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/next.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    dots: true,
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    vertical: true,
+                    verticalSwiping: true,
+                    arrows: false
+                }
+            }
+        ]
     });
 });
